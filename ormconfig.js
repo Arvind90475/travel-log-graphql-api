eec35b8113629bff4a471d3e54d69e1100d45121
arvind <arvind.bisht.90475@gmail.com>
2022-01-19 22:36:57 +1000

    fix migration

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,18 @@
 const isDevelopmentEnvironment = process.env.NODE_ENV === "development";
+
 const dbConfig = {
+  ...(isDevelopmentEnvironment
+    ? {
+        host: "localhost",
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+    : {
+        url: process.env.DATABASE_URL,
+      }),
   type: "postgres",
-  host: "localhost",
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   entities: ["dist/entities/**/*.js"],
   migrationsTableName: "migrations",
   migrations: ["dist/migrations/*.js"],
